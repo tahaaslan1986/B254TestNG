094a181d764279df4d54d4a4b2638edffa1767e5
Taha ASLAN <eng.tahaaslan@gmail.com>
2024-09-17 21:15:26 +0300

    tech3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,6 +6,7 @@ public class Runner {
         System.out.println("versiyon3");
         System.out.println("techpro 1");
         System.out.println("techpro 2");
+        System.out.println("techpro 3");
 
 
 

e6ade8362b799757a51a620eeeae080b1d864b0c
tahaaslan1986 <149255894+tahaaslan1986@users.noreply.github.com>
2024-09-17 22:14:06 +0300

    Update Runner.java

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,12 +1,14 @@
 public class Runner {
     public static void main(String[] args) {
 
+    
         System.out.println("versiyon1");
         System.out.println("versiyon2");
         System.out.println("versiyon3");
         System.out.println("techpro 1");
         System.out.println("techpro 2");
         System.out.println("techpro 3");
+        //remote da değişiklik
 
 
 
